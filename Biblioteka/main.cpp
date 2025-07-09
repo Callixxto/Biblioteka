6714e51878f6a1c5604c8505c6a39ed7f62460fc
Callixxto <san.stolarek@gmail.com>
2025-07-09 21:30:21 +0200

    Update main.cpp
    Improved main

--- Biblioteka/main.cpp
+++ Biblioteka/main.cpp
@@ -4,19 +4,22 @@
 int main() {
     Biblioteka b;
 
+
     b.dodajKsiazke(Ksiazka(1, "Hobbit", "J. R.R. Martin"));
     b.dodajKsiazke(Ksiazka(2, "Dark Rise", "C.S. Pascat"));
-	b.dodajKsiazke(Ksiazka(3, "AFTG", "Nora Sakavic"));
+    b.dodajKsiazke(Ksiazka(3, "AFTG", "Nora Sakavic"));
+
 
     b.wypozyczKsiazke(1, "Czytelnik 1");
-	b.wypozyczKsiazke(3, "Czytelnik 2");
+    b.wypozyczKsiazke(3, "Czytelnik 2");
     b.wyswietlStan();
 
+    // Zwrot i usunięcie książek
     b.zwrocKsiazke(1);
     b.usunKsiazke(2);
 
 
-	b.wypozyczKsiazke(3, "Czytelnik 3");
+    b.wypozyczKsiazke(3, "Czytelnik 3");
     b.wyswietlStan();
 
     return 0;
